adf0a502baaedbea18a2c531058a2ebad0350913
rootbsd <paul@r00ted.com>
2018-07-25 07:19:53 +0200

    Changes to be committed: new file:   dumper.py new file:   fridump3.py new file:   utils.py

---
+++ fridump3.py
@@ -0,0 +1,165 @@
+import textwrap
+import frida
+import os
+import sys
+import frida.core
+import dumper
+import utils
+import argparse
+import logging
+
+logo = """
+        ______    _     _
+        |  ___|  (_)   | |
+        | |_ _ __ _  __| |_   _ _ __ ___  _ __
+        |  _| '__| |/ _` | | | | '_ ` _ \| '_ \\
+        | | | |  | | (_| | |_| | | | | | | |_) |
+        \_| |_|  |_|\__,_|\__,_|_| |_| |_| .__/
+                                         | |
+                                         |_|
+        """
+
+
+# Main Menu
+def MENU():
+    parser = argparse.ArgumentParser(
+        prog='fridump',
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        description=textwrap.dedent(""))
+
+    parser.add_argument(
+        'process', help='the process that you will be injecting to')
+    parser.add_argument('-o', '--out', type=str, help='provide full output directory path. (def: \'dump\')',
+                        metavar="dir")
+    parser.add_argument('-u', '--usb', action='store_true',
+                        help='device connected over usb')
+    parser.add_argument('-H', '--host', type=str,
+                        help='device connected over IP')
+    parser.add_argument('-v', '--verbose', action='store_true', help='verbose')
+    parser.add_argument('-r', '--read-only', action='store_true',
+                        help="dump read-only parts of memory. More data, more errors")
+    parser.add_argument('-s', '--strings', action='store_true',
+                        help='run strings on all dump files. Saved in output dir.')
+    parser.add_argument('--max-size', type=int, help='maximum size of dump file in bytes (def: 20971520)',
+                        metavar="bytes")
+    args = parser.parse_args()
+    return args
+
+
+print(logo)
+
+arguments = MENU()
+
+# Define Configurations
+APP_NAME = utils.normalize_app_name(appName=arguments.process)
+DIRECTORY = ""
+USB = arguments.usb
+NETWORK=False
+DEBUG_LEVEL = logging.INFO
+STRINGS = arguments.strings
+MAX_SIZE = 20971520
+PERMS = 'rw-'
+
+if arguments.host is not None:
+  NETWORK=True
+  IP=arguments.host
+
+if arguments.read_only:
+    PERMS = 'r--'
+
+if arguments.verbose:
+    DEBUG_LEVEL = logging.DEBUG
+logging.basicConfig(format='%(levelname)s:%(message)s', level=DEBUG_LEVEL)
+
+
+# Start a new Session
+session = None
+try:
+    if USB:
+        session = frida.get_usb_device().attach(APP_NAME)
+    elif NETWORK:
+        session = frida.get_device_manager().add_remote_device(IP).attach(APP_NAME)
+    else:
+        session = frida.attach(APP_NAME)
+except Exception as e:
+    print(str(e))
+    sys.exit()
+
+
+# Selecting Output directory
+if arguments.out is not None:
+    DIRECTORY = arguments.out
+    if os.path.isdir(DIRECTORY):
+        print("Output directory is set to: " + DIRECTORY)
+    else:
+        print("The selected output directory does not exist!")
+        sys.exit(1)
+
+else:
+    print("Current Directory: " + str(os.getcwd()))
+    DIRECTORY = os.path.join(os.getcwd(), "dump")
+    print("Output directory is set to: " + DIRECTORY)
+    if not os.path.exists(DIRECTORY):
+        print("Creating directory...")
+        os.makedirs(DIRECTORY)
+
+mem_access_viol = ""
+
+print("Starting Memory dump...")
+
+def on_message(message, data):
+    print("[on_message] message:", message, "data:", data)
+
+
+script = session.create_script("""'use strict';
+
+rpc.exports = {
+  enumerateRanges: function (prot) {
+    return Process.enumerateRangesSync(prot);
+  },
+  readMemory: function (address, size) {
+    return Memory.readByteArray(ptr(address), size);
+  }
+};
+""")
+script.on("message", on_message)
+script.load()
+
+agent = script.exports
+ranges = agent.enumerate_ranges(PERMS)
+
+if arguments.max_size is not None:
+    MAX_SIZE = arguments.max_size
+
+i = 0
+l = len(ranges)
+
+# Performing the memory dump
+for range in ranges:
+    logging.debug("Base Address: " + str(range["base"]))
+    logging.debug("")
+    logging.debug("Size: " + str(range["size"]))
+    if range["size"] > MAX_SIZE:
+        logging.debug("Too big, splitting the dump into chunks")
+        mem_access_viol = dumper.splitter(
+            agent, range["base"], range["size"], MAX_SIZE, mem_access_viol, DIRECTORY)
+        continue
+    mem_access_viol = dumper.dump_to_file(
+        agent, range["base"], range["size"], mem_access_viol, DIRECTORY)
+    i += 1
+    utils.printProgress(i, l, prefix='Progress:', suffix='Complete', bar=50)
+
+# Run Strings if selected
+
+if STRINGS:
+    files = os.listdir(DIRECTORY)
+    i = 0
+    l = len(files)
+    print("Running strings on all files:")
+    for f1 in files:
+        utils.strings(f1, DIRECTORY)
+        i += 1
+        utils.printProgress(i, l, prefix='Progress:',
+                            suffix='Complete', bar=50)
+print("Finished!")
+#raw_input('Press Enter to exit...')
---
+++ utils.py
@@ -0,0 +1,39 @@
+import sys
+import string
+import logging
+import os
+import re
+
+# Progress bar function
+
+
+def printProgress(times, total, prefix='', suffix='', decimals=2, bar=100):
+    filled = int(round(bar * times / float(total)))
+    percents = round(100.00 * (times / float(total)), decimals)
+    bar = '#' * filled + '-' * (bar - filled)
+    sys.stdout.write('%s [%s] %s%s %s\r' %
+                     (prefix, bar, percents, '%', suffix)),
+    sys.stdout.flush()
+    if times == total:
+        print("\n")
+
+
+# A very basic implementations of Strings
+def strings(filename, directory, min=4):
+    strings_file = os.path.join(directory, "strings.txt")
+    path = os.path.join(directory, filename)
+    with open(path, encoding='Latin-1') as infile:
+        str_list = re.findall("[A-Za-z0-9/\-:;.,_$%'!()[\]<> \#]+", infile.read())
+        with open(strings_file, "a") as st:
+            for string in str_list:
+                if len(string) > min:
+                    logging.debug(string)
+                    st.write(string + "\n")
+
+# Normalize thw namo of application works better on frida
+def normalize_app_name(appName: str):
+    try:
+        appName = int(appName)
+    except ValueError:
+        pass
+    return appName
